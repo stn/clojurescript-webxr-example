052efc6aff4bb904ebfbc22a6e99915d990b74f3
Akira Ishino <akira.ishino@gmail.com>
2022-12-19 21:07:47 +0900

    three.js

--- src/main/clojurescript_webxr_example/app.cljs
+++ src/main/clojurescript_webxr_example/app.cljs
@@ -1,4 +1,24 @@
-(ns clojurescript-webxr-example.app)
+(ns clojurescript-webxr-example.app
+      (:require ["three" :as three]))
+
+(defonce scene (three/Scene.))
+(defonce camera (three/PerspectiveCamera. 75 (/ (.-innerWidth js/window) (.-innerHeight js/window)) 0.1 1000))
+(defonce renderer (doto (three/WebGLRenderer.)
+                        (.setSize (.-innerWidth js/window) (.-innerHeight js/window))
+                        (->> .-domElement
+                             (.appendChild (.-body js/document)))))
+
+(defonce geometry (three/BoxGeometry. 1 1 1))
+(defonce material (three/MeshBasicMaterial. #js {:color 0x00ff00}))
+(defonce cube (three/Mesh. geometry material))
+
+(defn animate []
+      (.requestAnimationFrame js/window animate)
+      (set! (.. cube -rotation -x) (+ (.. cube -rotation -x) 0.01))
+      (set! (.. cube -rotation -y) (+ (.. cube -rotation -y) 0.01))
+      (.render renderer scene camera))
 
 (defn init []
-      (println "Hello World"))
+      (.add scene cube)
+      (set! (.. camera -position -z) 5)
+      (animate))
